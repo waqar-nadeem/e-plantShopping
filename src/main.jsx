58b19f80d3adef04f39f06092e83261f8fbe260f
Waqar Nadeem <nadeemwaqar215@gmail.com>
2025-08-15 10:50:23 -0400

    Fix layout overflow, update App.css for responsive design, finalize Redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Import the main App component
+import App from './App.jsx';
+
+// Import global styles
+import './index.css';
+
+// Import Provider from react-redux to connect Redux with React
+import { Provider } from 'react-redux';
+
+// Import the configured Redux store
+import store from './store.js';
+
+// Render the App component wrapped with Provider to enable global state access
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
+// Import the configureStore function from Redux Toolkit
 import { configureStore } from '@reduxjs/toolkit';
+
+// Import the cartReducer from your CartSlice file
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Create a Redux store using configureStore
+const store = configureStore({
+  // Define the root reducer object
+  reducer: {
+    // 'cart' is the name of the slice in the store, and it's managed by cartReducer
+    cart: cartReducer,
+  },
 });
-export default store
+
+// Export the store for use in the app (e.g., in <Provider store={store}>)
+export default store;
